2c55cc2886f6c0b7d690af543de53135e5ebfd40
AliouacheneAnis <eaalioua01@monccnb.ca>
2022-10-28 14:25:21 -0300

    Version Final

--- include/Secrets.h
+++ include/Secrets.h
@@ -3,11 +3,11 @@ SSID du WIFI ainsi que le mot de passe
 Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
 */
 
-#define WIFI_SECRET_SSID "Ste-adele"
-#define WIFI_SECRET_PASS "allo1234"
+//#define WIFI_SECRET_SSID "Ste-adele"
+//#define WIFI_SECRET_PASS "allo1234"
 
-//#define WIFI_SECRET_SSID "UNIFI_IDO2"
-//#define WIFI_SECRET_PASS "99Bidules!"
+#define WIFI_SECRET_SSID "UNIFI_IDO2"
+#define WIFI_SECRET_PASS "99Bidules!"
 
 //#define WIFI_SECRET_SSID "Note10"
 //#define WIFI_SECRET_PASS "abcd7593"
@@ -17,8 +17,8 @@ Information nécessaire pour le branchement a un Broker MQTT
 Dans le cadre du cours AIIA1013, ne modifié pas cet information
 */
 
-#define SECRET_MQTT_SERVER_IP "10.0.0.163"
-//#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+//#define SECRET_MQTT_SERVER_IP "10.0.0.163"
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
 #define SECRET_MQTT_SERVER_PORT 1883
 
 /*
@@ -26,8 +26,8 @@ Détails sur l'indentification de l'objet
 Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
 */
 
-#define SECRET_TOKEN  "ISayoH1CMB80rE1cuExU"
-#define SECRET_DEVICE_ID "f8d878e0-3f60-11ed-93e1-d9033df874b9"
+#define SECRET_TOKEN  "WzwYMvGEblYuxteu91Zr"
+#define SECRET_DEVICE_ID "acd5d960-5392-11ed-b0d9-173201e7440f"
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 /*
-  Titre      : Capture des données sur une carte SD
+  Titre      : Systeme de climatisation 
   Auteur     : Anis Aliouachene
-  Date       : 27/09/2022
+  Date       : 28/10/2022
   Description: 
-              - Capture du TimeStamp avec le RTC (Date,Heure, Minutes, Seconde), ainsi que la Température et humidité avec le AHT20
-              - Ces données doivent être capturé a toutes les 5 secondes et conservé sur la carte SD
-              - Transmission de ces données a toutes les minutes sur Thingsboard
-              - Une fois les données transmise, effacé les données de la carte SD
-              - Vérifié le résultat dans les données de télémétries
-  Version    : 0.0.2
+              - Stockage de la dateheure, température et humidité sur la carte SD a tout les 5 secondes provenant du AHT20
+              - Transfert de ces données a tout les 30 secondes en utilisant le format nécessaire pour que Thingsboard utiliser la dateheure de la capture des données
+              - Notification de température élevé ou faible avec Rule Chain (a reproduire sur le serveur de production)
+              - RPC (LED bleu pour température faible, LED Rouge pour température élevé)  avec Rule Chain
+              - Remplacé la LED Bleu et Rouge par un servo moteur
+                      Rotation vers la gauche (Chauffage – LED Bleu)
+                      Rotation vers la droite (Climatisation – LED Rouge)
+Version    : 0.0.2
 */
 
 
@@ -25,29 +27,25 @@
 
 RTC_DS3231 rtc;      // Declration object rtc
 Adafruit_AHTX0 aht;  // Declaratiob object aht
-Servo myservo;
+Servo myservo;       // Declaration Object myservo 
 
 const int CHIP_SELECT = 4, SERVO = 1;  // chipselect pour fonctionner le mem shield 
 const unsigned long DELAY_TIME = 5000, DELAY_TIME_ENVOI = 30000, DELAY_RPC = 2000;  // Temps d'attente 
-const int LED_PIN_ROUGE = 2, LED_PIN_BLUE = 3, IntensiteAllumer = 255, IntensiteEteint = 0;  
 
 sensors_event_t Tmp, Hum;   // Declaration evenement 
-String Seconde, Minute, Heure, Jour, Mois, Annee, DataString, DataEnvoi,Data;
+String DataString, DataEnvoi,Data; // String pour les chaine MQTT et RPC 
 
+// Variables
 long AnalogValue; 
 unsigned long  TempsAvant = 0, TempsAvantEnvoi = 0,TempsRPC = 0; 
 int CurrentVal, Position = 90;  
 
 void setup() {
   
-  myservo.attach(SERVO);
-  
+  myservo.attach(SERVO); // attach servo au PIN 1
+
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-
-  pinMode(LED_PIN_BLUE,OUTPUT);
-  pinMode(LED_PIN_ROUGE,OUTPUT); 
-  pinMode(LED_BUILTIN,OUTPUT); 
 
   Serial.print("Initializing SD card...");
   // see if the card is present and can be initialized:
@@ -80,12 +78,12 @@ void loop() {
   if (millis() - TempsAvant > DELAY_TIME )
   
   {   
-      aht.getEvent(&Hum, &Tmp);// populate temp and humidity objects with fresh data
+      aht.getEvent(&Hum, &Tmp); // populate temp and humidity objects with fresh data
 
       DateTime now = rtc.now(); // Declaration object Now qui recois les donnees actuelle de la part de l'object rtc 
       
       // Creation de la chaine de caractere 
-      DataString = "{\"ts\": " + String(now.unixtime()+ 14400 ) + "000, \"values\":{\"tmp\": " + String(Tmp.temperature) + ", \"Hum\": " + String(Hum.relative_humidity) + "}}F";
+      DataString = "{\"ts\": " + String(now.unixtime()+ 18480 ) + "000, \"values\":{\"tmp\": " + String(Tmp.temperature) + ", \"Hum\": " + String(Hum.relative_humidity) + "}}F";
 
       // open the file. note that only one file can be open at a time,
       // so you have to close this one before opening another.
@@ -108,14 +106,15 @@ void loop() {
 
   if ( millis() - TempsAvantEnvoi > DELAY_TIME_ENVOI )
   { 
+    // Connexion au wifi pour l'envoi 
     status = WL_IDLE_STATUS; 
     digitalWrite(LED_BUILTIN, HIGH);
     wifiConnect(); 
     MQTTConnect();
-
+    
     unsigned int i = 0; 
     File dataFile = SD.open("datalog.txt", FILE_READ);
-        // if the file is available, write to it:
+    // if the file is available, write to it:
     if (dataFile) {
         
         while (i < dataFile.size()-1) // Boucle pour lire le fichier jusqu'a la fin  selon i 
@@ -134,6 +133,7 @@ void loop() {
         Serial.println("error opening datalog.txt");
       } 
       
+      // Fermeture de fichier apres l'envoi vers thingsboard et reception message RPC 
       dataFile.close(); 
       SD.remove("datalog.txt");
       ClientMQTT.loop();
@@ -143,22 +143,16 @@ void loop() {
           {
             if (DataRecu == "Clim")
             {
-                analogWrite(LED_PIN_ROUGE, IntensiteAllumer);
-                Serial.println("LED Rouge Allumer"); 
                 Position = 180;   
                 myservo.write(Position); // Servo moteur vers la position 180
 
             }else if (DataRecu == "Chauffage")
             {
-                analogWrite(LED_PIN_BLUE, IntensiteAllumer);
-                Serial.println("LED Blue Allmer"); 
                 Position = 0;   
                 myservo.write(Position); // Servo moteur vers la position 0
 
             }else{ 
-                  analogWrite(LED_PIN_ROUGE, IntensiteEteint);
-                  analogWrite(LED_PIN_BLUE, IntensiteEteint);
-                  Serial.println("pasDatarecu");
+
                   Position = 90;   
                   myservo.write(Position); // Servo moteur vers la position 0
             }
@@ -166,6 +160,7 @@ void loop() {
             Data = DataRecu;
         }
       
+      // deconnexion du wifi et MQTT 
       Serial.println("\n prochain Lot .... "); 
       ClientMQTT.disconnect();
       WiFi.disconnect();
